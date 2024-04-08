c8aff4e861d1ba0e4a22312ae06ea43dc49cf0f6
TwinIsland <twisland@outlook.com>
2024-04-07 22:26:29 -0500

    fix minor bug

--- utils/config_loader.py
+++ utils/config_loader.py
@@ -1,9 +1,11 @@
 import configparser
 
-CONFIG_DIR = '../sql_server.ini'
+CONFIG_DIR = './sql_server.ini'
 
 
 class Config:
     def __init__(self):
         self.config = configparser.ConfigParser()
         self.config.read(CONFIG_DIR)
+
+        print(self.config.sections())
